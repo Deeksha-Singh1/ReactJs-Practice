0bc6d1da67b0a9002a018ad258c89515a2a77db4
Deeksha-Singh1 <deeksha.singh1_cs19@gla.ac.in>
2023-05-21 14:30:07 +0530

    react js practice

---
+++ myapp/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function MyComponenet(){
+  return <div> my component</div>
+} 
+
+function App() {
+  return (
+  <div>Hello Guys
+    <MyComponenet/>
+  </div>
+  );
+}
+
+
+
+export default App;
